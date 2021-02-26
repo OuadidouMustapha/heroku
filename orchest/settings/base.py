ead4e8f1ddf3dc2503f63325405f76cc7320a50d
ouadidou <mustapha.o@gclgroup.com>
2021-02-26 16:27:08 +0100

    heroku

--- orchest/settings/base.py
+++ orchest/settings/base.py
@@ -332,12 +332,16 @@ LOGGING = {
     }
 }
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Activate Django-Heroku.
 django_on_heroku.settings(locals())
